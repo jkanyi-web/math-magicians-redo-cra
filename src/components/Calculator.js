0c4acae4e992041c1e4dce70909c5ae3532cb33b
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-09-12 16:04:17 +0300

    Add Calculator child component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,31 +1,12 @@
 import CalculatorInput from './CalculatorInput';
+import ItemsContainer from './ItemsContainer';
 
 export default function Calculator() {
   return (
     <>
       <div className="container">
         <CalculatorInput />
-        <div className="itemsContainer">
-          <button type="button" className="items">+/-</button>
-          <button type="button" className="items">%</button>
-          <button type="button" className="items">AC</button>
-          <button type="button" className="items orange">÷</button>
-          <button type="button" className="items">7</button>
-          <button type="button" className="items">8</button>
-          <button type="button" className="items">9</button>
-          <button type="button" className="items orange">*</button>
-          <button type="button" className="items">4</button>
-          <button type="button" className="items">5</button>
-          <button type="button" className="items">6</button>
-          <button type="button" className="items orange">-</button>
-          <button type="button" className="items">1</button>
-          <button type="button" className="items">2</button>
-          <button type="button" className="items">3</button>
-          <button type="button" className="items orange">+</button>
-          <button type="button" className="items long">0</button>
-          <button type="button" className="items">.</button>
-          <button type="button" className="items orange">=</button>
-        </div>
+        <ItemsContainer />
       </div>
     </>
   );
