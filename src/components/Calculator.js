f19c798d53014af0398ac865ddf86cfb8163eac4
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-09-13 16:13:04 +0300

    Complete calculator functionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,84 @@
-import CalculatorInput from './CalculatorInput';
-import ItemsContainer from './ItemsContainer';
+import { useState } from 'react';
+import calculate from '../logic/calculate';
 
 export default function Calculator() {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const buttonClick = (e) => {
+    setState(calculate(state, e.target.textContent));
+  };
+
   return (
-    <>
-      <div className="container">
-        <CalculatorInput />
-        <ItemsContainer />
+    <section className="container">
+      <input
+        type="text"
+        className="input"
+        readOnly
+        value={state.next || state.total || '0'}
+      />
+      <div className="itemsContainer">
+        <button type="button" onClick={buttonClick} className="items">
+          AC
+        </button>
+        <button type="button" onClick={buttonClick} className="items">
+          +/-
+        </button>
+        <button type="button" onClick={buttonClick} className="items">
+          %
+        </button>
+        <button type="button" onClick={buttonClick} className="items orange">
+          ÷
+        </button>
+        <button type="button" onClick={buttonClick} className="items">
+          7
+        </button>
+        <button type="button" onClick={buttonClick} className="items">
+          8
+        </button>
+        <button type="button" onClick={buttonClick} className="items">
+          9
+        </button>
+        <button type="button" onClick={buttonClick} className="items orange">
+          x
+        </button>
+        <button type="button" onClick={buttonClick} className="items">
+          4
+        </button>
+        <button type="button" onClick={buttonClick} className="items">
+          5
+        </button>
+        <button type="button" onClick={buttonClick} className="items">
+          6
+        </button>
+        <button type="button" onClick={buttonClick} className="items orange">
+          -
+        </button>
+        <button type="button" onClick={buttonClick} className="items">
+          1
+        </button>
+        <button type="button" onClick={buttonClick} className="items">
+          2
+        </button>
+        <button type="button" onClick={buttonClick} className="items">
+          3
+        </button>
+        <button type="button" onClick={buttonClick} className="items orange">
+          +
+        </button>
+        <button type="button" onClick={buttonClick} className="items long">
+          0
+        </button>
+        <button type="button" onClick={buttonClick} className="items">
+          .
+        </button>
+        <button type="button" onClick={buttonClick} className="items orange">
+          =
+        </button>
       </div>
-    </>
+    </section>
   );
 }
