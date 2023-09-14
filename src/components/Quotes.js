4b20a4a04fb7c43029d2020f389720c6d986f1e4
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-09-14 13:22:52 +0300

    small adjustments to optimise code

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,33 +1,31 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-axios.defaults.headers.get['X-Api-Key'] = 'X0RJf1RjhctUxTka7UQisg==TgVq8OcBO5Lh3LUw';
+axios.defaults.headers.common['X-Api-Key'] = 'X0RJf1RjhctUxTka7UQisg==TgVq8OcBO5Lh3LUw';
 
 export default function Quotes() {
-  const [loading, setLoading] = useState('Loading...');
+  const [quote, setQuote] = useState('Loading...');
   const [error, setError] = useState('');
 
-  const FetchQuotes = () => {
-    axios
-      .get('https://api.api-ninjas.com/v1/quotes?category=success')
-      .then((res) => {
-        if (res.status === 200) {
-          setLoading(res.data[0].quote);
-        }
-      })
-      .catch((e) => {
-        setError(e.message);
-      });
-  };
-
   useEffect(() => {
-    FetchQuotes();
+    const fetchQuote = async () => {
+      try {
+        const response = await axios.get('https://api.api-ninjas.com/v1/quotes?category=success');
+        if (response.status === 200) {
+          setQuote(response.data[0].quote);
+        }
+      } catch (e) {
+        setError('An error occurred while fetching the quote.');
+      }
+    };
+
+    fetchQuote();
   }, []);
 
   return (
     <div>
-      <h1>{loading}</h1>
-      <h3>{error}</h3>
+      <h1>{quote}</h1>
+      {error && <h3>{error}</h3>}
     </div>
   );
 }
